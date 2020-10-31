6d34183e613de8f259877fc834318db1f1346a86
Kushal-Ajay-Shah <shahkushal0602@gmail.com>
2020-10-31 18:54:27 +0530

    comments added

--- task_1b.py
+++ task_1b.py
@@ -17,10 +17,11 @@
 *****************************************************************************************
 '''
 
-# Team ID:			[ Team-ID ]
-# Author List:		[ Names of team members worked on this file separated by Comma: Name1, Name2, ... ]
+# Team ID:			[ 1631 ]
+# Author List:		[ Names of team members worked on this file separated by Comma: Kushal Shah, Dushant Panchbhai ]
 # Filename:			task_1b.py
-# Functions:		applyPerspectiveTransform, detectMaze, writeToCsv
+# Functions:		applyPerspectiveTransform, detectMaze, writeToCsv, preProcess, pP1,
+#					reorder, detect_wall
 # 					[ Comma separated list of functions in this file ]
 # Global variables:	
 # 					[ List of global variables defined in this file ]
@@ -42,18 +43,15 @@ import csv
 ## Please add proper comments to ensure that your code is   ##
 ## readable and easy to understand.                         ##
 ##############################################################
+
+# Function to preprocess the image
 def preProcess(img):
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     imgBlur = cv2.GaussianBlur(imgGray, (5,5), 1)
     imgThreshold = cv2.adaptiveThreshold(imgBlur, 255, 1, 1, 11, -2)
     return imgThreshold
 
-def pP(img):
-    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    imgBlur = cv2.medianBlur(imgGray, 5)
-    imgThreshold = cv2.adaptiveThreshold(imgBlur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-    return imgThreshold
-
+# Another preprocessing function using different thresholding and kernel
 def pP1(img):
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     kernel = np.ones((5,5),np.float32)/25
@@ -61,7 +59,7 @@ def pP1(img):
     imgThreshold = cv2.adaptiveThreshold(imgBlur, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
     return imgThreshold
 
-
+# Function to reorder the points to avoid rotation of image
 def reorder(myPoints):
     myPoints = myPoints.reshape((4, 2))
     myPointsNew = np.zeros((4, 1, 2), dtype = np.float32)
@@ -74,87 +72,30 @@ def reorder(myPoints):
     
     return myPointsNew
 
-def vertical_wall(pimg, r , sci, eci):
-    prepart1 = pP(pimg)
+# Function to detect the wall
+def detect_wall(pimg):
+    prepart1 = preProcess(pimg) # preprocessed the image
 
-    cnt, hierarchy = cv2.findContours(prepart1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cnt, _ = cv2.findContours(prepart1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    good_c = []
+    good_c = [] # list to include filtered out the contours
     for c in cnt:
         area = cv2.contourArea(c)
-        if area > 50:
-            peri = cv2.arcLength(c, True)
-            approx = cv2.approxPolyDP(c, 0.02 * peri, True)
-            if (len(approx) == 4 or len(approx) == 5 or len(approx) == 6 ):
-                good_c.append(c)
-    
-    if(len(good_c) == 0):
-        prepart1 = preProcess(pimg)
+        if area > 700:
+            good_c.append(c)
 
-        cnt, hierarchy = cv2.findContours(prepart1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        
-        for c in cnt:
-            area = cv2.contourArea(c)
-            if area > 50:
-                peri = cv2.arcLength(c, True)
-                approx = cv2.approxPolyDP(c, 0.02 * peri, True)
-                if (len(approx) == 4 or len(approx) == 5 or len(approx) == 6 or len(approx) == 7):
-                    good_c.append(c)
-                
-    
+    # Now sorting the list in descending order according to their areas
     good_c.sort(reverse = True, key = lambda x : cv2.contourArea(x))
-    if len(good_c) != 0:
-        area_good = cv2.contourArea(good_c[0])
-        if area_good > 2300:
-            return False
-        else:
-            return True
+
+    area_good = cv2.contourArea(good_c[0])
+
+	# Bigger contour had area greater than 2250 so checking the areas accordingly
+	# if the area is less than this it means that the contours detected are smaller due 
+	# to a wall present between the 2 boxes
+    if area_good > 2250:
+        return False
     else:
-        return False  
-
-
-def horizontal_wall(pimg, c , sci, eci):
-    prepart1 = pP(pimg)
-
-    cnt, hierarchy = cv2.findContours(prepart1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-    good_c = []
-    for c in cnt:
-        area = cv2.contourArea(c)
-        if area > 50:
-            peri = cv2.arcLength(c, True)
-            approx = cv2.approxPolyDP(c, 0.02 * peri, True)
-            
-            
-            if (len(approx) == 4 or len(approx) == 5 or len(approx) == 6):
-                good_c.append(c)
-    
-    if(len(good_c) == 0):
-        prepart1 = preProcess(pimg)
-        
-        cnt, hierarchy = cv2.findContours(prepart1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        
-        for c in cnt:
-            area = cv2.contourArea(c)
-            if area > 50:
-                peri = cv2.arcLength(c, True)
-                approx = cv2.approxPolyDP(c, 0.02 * peri, True)
-                if (len(approx) == 4 or len(approx) == 5 or len(approx) == 6 or len(approx) == 7 or len(approx) == 8):
-                    good_c.append(c)
-        
-    good_c.sort(reverse = True, key = lambda x : cv2.contourArea(x))
-    if len(good_c) != 0:
-        area_good = cv2.contourArea(good_c[0])
-        if area_good > 2300:
-            return False
-        else:
-            return True
-    else:
-        return False    
-
-
-
-
+        return True 
 
 
 ##############################################################
@@ -185,14 +126,15 @@ def applyPerspectiveTransform(input_img):
 	warped_img = None
 
 	##############	ADD YOUR CODE HERE	##############
-	imgThreshold = pP1(input_img)
-	contours, hierarchy = cv2.findContours(imgThreshold, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
+	imgThreshold = pP1(input_img) # preprocessing the image
+	contours, _ = cv2.findContours(imgThreshold, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
 
+	# We need to find the second largest contour as the largest contour had edges of the image
+	# itself. So using for loops twice to find second largest areawise contour
 
 	max_area = 0
 	second_last_area = 0
 	second_big = np.array([])
-	biggest = np.array([])
 	for i in contours:
 		
 		area = cv2.contourArea(i)
@@ -200,7 +142,6 @@ def applyPerspectiveTransform(input_img):
 			peri = cv2.arcLength(i, True)
 			approx = cv2.approxPolyDP(i, 0.02 * peri, True)
 			if area > max_area and len(approx) == 4:
-				biggest = approx
 				max_area = area
 
 				
@@ -216,12 +157,14 @@ def applyPerspectiveTransform(input_img):
 					second_last_area = area
 				
 
-	second_big = reorder(second_big)
+	second_big = reorder(second_big) # reordering the points to avoid rotation
 	second_big = np.int0(second_big)
 
 	pts1 = np.float32(second_big)
 	pts2 = np.float32([[0,0], [500, 0], [0,500], [500,500]])
-	matrix = cv2.getPerspectiveTransform(pts1, pts2)
+
+    # Now doing perspective transform
+	matrix = cv2.getPerspectiveTransform(pts1, pts2) 
 	warped_img = cv2.warpPerspective(input_img, matrix, (500, 500))
 
 	
@@ -256,20 +199,26 @@ def detectMaze(warped_img):
 	maze_array = []
 
 	##############	ADD YOUR CODE HERE	##############
+    # arr_boxes is a numpy array filled with zeros
 	arr_boxes = np.zeros(shape=(10,10), dtype = int)
+
+	# Initializing the array as outer borders were present in all images
 	for i in range(10):
 		arr_boxes[i,0] += 1
 		arr_boxes[0,i] += 2
 		arr_boxes[i,9] += 4
 		arr_boxes[9,i] += 8
 
+    # Slicing the images to have a 50x100 part of image to detect if vertical wall is there
+	# between them 
+	# r is the row index
 	for r in range(10):
-		c = 0
-		sci = 0
-		eci = 100
+		c = 0   # column index
+		sci = 0  # starting column pixel value
+		eci = 100  # ending column pixel value
 		while(c != 9):  
 			part = warped_img[r * 50: r * 50 + 50 , sci:eci]
-			vw = vertical_wall(part, r , sci, eci)
+			vw = detect_wall(part)
 			if(vw):
 				arr_boxes[r,c] += 4
 				arr_boxes[r,c+1] += 1
@@ -277,13 +226,16 @@ def detectMaze(warped_img):
 			eci += 50
 			c += 1
 
+    # Slicing the images to have a 100x50 part of image to detect if vertical wall is there
+	# between them 
+	# c is the column index
 	for c in range(10):
-		r = 0
-		sri = 0
-		eri = 100
+		r = 0    # row index
+		sri = 0  # starting row pixel value
+		eri = 100  # ending row pixel value
 		while(r != 9):  
 			part = warped_img[sri:eri ,c * 50: c * 50 + 50]
-			hw = horizontal_wall(part, c , sri, eri)
+			hw = detect_wall(part)
 			if(hw):
 				arr_boxes[r,c] += 8
 				arr_boxes[r+1,c] += 2
@@ -291,6 +243,7 @@ def detectMaze(warped_img):
 			eri += 50
 			r += 1
 
+    # Finally converting numpy array to list
 	maze_array = arr_boxes.tolist()
 
 	
